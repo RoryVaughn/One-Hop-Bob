e851c4c80165c38d81b03d02e42eaee1facb810f
Rory Vaughn <Rvaughn2525@gmail.com>
2018-11-15 19:43:38 -0600

    small menu edits

--- One Hop Bob/Assets/Scripts/SettingsMenu.cs
+++ One Hop Bob/Assets/Scripts/SettingsMenu.cs
@@ -39,6 +39,8 @@ public class SettingsMenu : MonoBehaviour {
         resolutionDropdown.AddOptions(choices);
         resolutionDropdown.value = currentRes;
         resolutionDropdown.RefreshShownValue();
+
+
     }
 
     public void PlayGame()
