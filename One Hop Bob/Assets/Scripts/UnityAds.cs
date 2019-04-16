19cf9657d62da1331999d07199cbe2a5fbe68c48
Rory Vaughn <Rvaughn2525@gmail.com>
2019-04-16 17:12:59 -0500

    reward ad button added

--- One Hop Bob/Assets/Scripts/UnityAds.cs
+++ One Hop Bob/Assets/Scripts/UnityAds.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Monetization;
-using UnityEngine.Advertisements;
 
 public class UnityAds : MonoBehaviour
 {
+
+    public string placementId2 = "rewardedVideo";
+
+    public void ShowRewardAd()
+    {
+        StartCoroutine(WaitForAd());
+    }
+
+    IEnumerator WaitForAd()
+    {
+        while (!Monetization.IsReady(placementId2))
+        {
+            yield return null;
+        }
+
+        ShowAdPlacementContent ad = null;
+        ad = Monetization.GetPlacementContent(placementId2) as ShowAdPlacementContent;
+
+        if (ad != null)
+        {
+            ad.Show(AdFinished);
+        }
+    }
+
+    void AdFinished(ShowResult result)
+    {
+        if (result == ShowResult.Finished)
+        {
+            // Reward the player
+        }
+    }
     public string placementId = "video";
 
     public void ShowAd()
@@ -31,6 +61,8 @@ public class UnityAds : MonoBehaviour
 
         string gameId = "3116483";
     bool testMode = true;
+
+
 
     void Start()
     {
